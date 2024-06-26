a7d88009151bc20a0d365b3dfd0e880a7965bdee
Sergey Pustovalov <pustserg@yandex.ru>
2024-06-26 09:52:25 +0200

    First version

---
+++ src/app/partials/_main_content.tsx
@@ -0,0 +1,52 @@
+import { promises as fs } from 'fs';
+
+export default async function MainContent() {
+  const file = await fs.readFile(process.cwd() + '/src/data/experience.json', 'utf-8');
+  const data = JSON.parse(file);
+
+  type Experience = {
+    company: string;
+    company_url: string;
+    position: string;
+    work_period: string;
+    description: string;
+  }
+
+  return (
+    <div className="grid grid-cols-1 divide divide-y-2">
+      <div className=" border-solid border-black border-b-1 py-4">
+        <p className="text-xl font-bold">
+          Key Skills
+        </p>
+        <ul className="list-none">
+          <li>Programming Languages & Frameworks: Ruby, Elixir, Golang, Ruby on Rails, Grape</li>
+          <li>Background Processing: Sidekiq, Sneakers</li>
+          <li>Databases: PostgreSQL, Mysql, MongoDB</li>
+          <li>Message Brokers: Kafka, RabbitMQ, AWS SQS</li>
+          <li>Data Serialization: Protobuf, gRPC</li>
+          <li>DevOps: Docker, AWS, Ansible</li>
+        </ul>
+      </div>
+      <div className="py-4 border-solid border-black border-b-2">
+        <p className="text-xl font-bold">Experience</p>
+        {data.map((item: Experience, index: number) => (
+          <div key={index} className="py-4">
+            { item.company_url === ""
+              ? <p>{item.company}</p>
+              : <a className="underline hover:no-underline" href={item.company_url} target="_blank" rel="noopener noreferrer">
+                  {item.company}
+                </a>
+            }
+            <p>{item.position}</p>
+            <p>{item.work_period}</p>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
+      <div className="py-4 border-solid border-black border-b-2">
+        <p className="text-xl font-bold">Hobbies</p>
+        <p>Reading, Music, Coding, Snowboarding, Ice Hockey</p>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/partials/_sidebar.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLinkedin, faGithub, faTelegram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; import fontawesome from '@fortawesome/fontawesome'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+fontawesome.config.autoAddCss = false;
+
+library.add(faLinkedin, faGithub, faEnvelope, faTelegram);
+
+export default function Sidebar() {
+  return (
+    <>
+      <p className="text-center">
+        <Image src="/avatar.jpg" alt="Sergey Pustovalov" width={300} height={300} className="rounded-full" />
+      </p>
+      <p className="pt-16 text-lg">
+        Hi, my name Sergey Pustovalov.
+        <br />
+        I am a backend software engineer based in Amsterdam.
+        <br />
+        I love to make great things and solve complex problems.
+        <br />
+        My main programming language is Ruby. Also I am interested in Elixir and Golang.
+      </p>
+      <p className="pt-2">
+        <a href="https://linkedin.com/in/pustserg" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={['fab', 'linkedin']} fixedWidth size="2x" />
+        </a>
+        <a href="https://github.com/pustserg" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={['fab', 'github']} fixedWidth size="2x" />
+        </a>
+        <a href="https://t.me/pustserg" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={['fab', 'telegram']} fixedWidth size="2x" />
+        </a>
+        <a href="mailto:pustserg@me.com">
+          <FontAwesomeIcon icon={['fas', 'envelope']} fixedWidth size="2x" />
+        </a>
+      </p>
+    </>
+  )
+}
